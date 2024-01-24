f65bfa4869b616590dd4df9b930c9ae4cb5d33d3
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2024-01-24 10:30:33 +0700

    Update bai8.js

--- ss34/bai8.js
+++ ss34/bai8.js
@@ -1,7 +1,1 @@
-let deleteButtons = document.querySelectorAll('.delete');
-deleteButtons.forEach(function(button) {
-    button.onclick = function() {
-        let row = this.parentNode.parentNode; 
-        row.parentNode.removeChild(row);
-    };
-});
+
