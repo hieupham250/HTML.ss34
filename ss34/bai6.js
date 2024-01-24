8d86419c4476336e47cf11737df0a3f60eb4d8d6
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2024-01-24 14:13:43 +0700

    Update bai6.js

--- ss34/bai6.js
+++ ss34/bai6.js
@@ -1,9 +1,9 @@
 let input = document.getElementById("input");
 let btn = document.getElementById("btn");
-let ul = document.createElement("ul");
+let ul = document.getElementById("list");
 btn.onclick = function () {
     let text = document.createElement("li");
-    text.innerHTML = document.getElementById("input").value;
-    document.getElementById("list").appendChild(text);
-    document.getElementById("input").value = "";
+    text.innerHTML = input.value;
+    ul.appendChild(text);
+    input.value = "";
 }
